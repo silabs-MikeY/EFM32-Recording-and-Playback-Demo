b561ee8b69f401dc58ae63296aabfa702f325bd7
Michael Yablonovitch <miyablon@silabs.com>
2022-01-26 16:20:52 -0500

    removed build folder

--- GNU ARM v10.2.1 - Debug/gecko_sdk_3.2.3/platform/service/system/src/sl_system_process_action.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_3.2.3/platform/service/system/src/sl_system_process_action.o: \
- C\:/SiliconLabs/SimplicityStudio/v5/developer/sdks/gecko_sdk_suite/v3.2/platform/service/system/src/sl_system_process_action.c \
- c\:\siliconlabs\simplicitystudio\v5\developer\sdks\gecko_sdk_suite\v3.2\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C\:\Users\MIYABLON\SimplicityStudio\v5_workspace\Recording_LDMA\autogen/sl_event_handler.h
-c\:\siliconlabs\simplicitystudio\v5\developer\sdks\gecko_sdk_suite\v3.2\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C\:\Users\MIYABLON\SimplicityStudio\v5_workspace\Recording_LDMA\autogen/sl_event_handler.h:
